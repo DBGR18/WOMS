552b11f67e18df1940b41d793f4d209fb5ef95f6
AlenChen0216 <alen0216@gmail.com>
2026-05-17 16:48:20 +0800

    Fix the nginx resolver problem and checked the hpa work well.

--- web/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# 從 /etc/resolv.conf 取得 Kubernetes CoreDNS 的 ClusterIP
-export NGINX_RESOLVER=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
-echo "NGINX_RESOLVER set to: ${NGINX_RESOLVER}"
